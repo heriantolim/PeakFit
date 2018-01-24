18bcb2bcc5a03c3c652466d4abf71305c4a37ae5
Herianto Lim <mail@heriantolim.com>
2018-01-24 12:09:42 +1100

    Tidy up
    Fix some comments.

--- @PeakFit/PeakFit.m
+++ @PeakFit/PeakFit.m
@@ -48,8 +48,8 @@ classdef PeakFit
 %
 %  XData: The X data points of the curve.
 %
-%  YData: The Y data points of the curve. The peak fitting may not properly if
-%         some Y data points are negative.
+%  YData: The Y data points of the curve. The peak fitting may not work properly
+%         if some Y data points are negative.
 %
 %  Window: A vector of length two [a,b] that limits the fitting to only the
 %          data points whose X coordinates lies within [a,b].
@@ -72,58 +72,61 @@ classdef PeakFit
 %             'Lorentzian'. If PeakShape contains only one element, then the
 %             default value is the same as that element.
 %
+%  BaselinePolyOrder: An integer that specifies the order of the polynomial
+%                     function used to fit the background of the spectrum.
+%                     Defaults to 0 (a constant polynomial). Set this to a
+%                     negative value to exclude the polynomial from the fit
+%                     model.
+%
 %  ...Start: A vector of initial values for the ... coefficients, where the
-%            blanks are one of the {Area,Center,Width,Height,Baseline}.
-%            The default values are determined heuristically. To make certain
-%            elements default, use NaN. They will be then replaced with the
-%            default values upon fitting.
+%            blanks are one of the {Area, Center, Width, Height, Baseline}. The
+%            default values are determined heuristically. To make certain
+%            properties default, set their values to NaN. They will be then
+%            replaced with the default values upon fitting.
 %
-%  ...Low: A vector of lower bounds on the ... coefficients, where the
-%          blanks are one of the {Area,Center,Width,Height,Baseline}.
-%          The default values are determined heuristically. To make certain
-%          elements default, use -Inf. They will be then replaced with the
-%          default values upon fitting.
+%  ...Low: A vector of lower bounds for the ... coefficients, where the blanks
+%          are one of the {Area, Center, Width, Height, Baseline}. The default
+%          values are determined heuristically. To make certain properties
+%          default, set their values to -Inf. They will be then replaced with
+%          the default values upon fitting.
 %
-%  ...Up: A vector of upper bounds on the ... coefficients, where the
-%         blanks are one of the {Area,Center,Width,Height,Baseline}.
-%         The default values are determined heuristically. To make certain
-%         elements default, use Inf. They will be then replaced with the default
-%         values upon fitting.
+%  ...Up: A vector of upper bounds for the ... coefficients, where the blanks
+%         are one of the {Area, Center, Width, Height, Baseline}. The default
+%         values are determined heuristically. To make certain properties
+%         default, set their values to Inf. They will be then replaced with the
+%         default values upon fitting.
 %
-%  Robust: Specifies the robust linear least-squares fitting method to be used.
-%          Avaliable values are 'on', 'off', 'LAR', or 'Bisquare'. The default
-%          is 'off'. 'LAR' specifies the least absolute residual method and
-%          'Bisquare' specifies the bisquare weights method. 'on' is equivalent
-%          to 'Bisquare', the default method.
+%  Robust: The type of the least-squares method to be used in the fitting.
+%          Available values are 'off', 'LAR' (least absolute residual method),
+%          and 'Bisquare' (bisquare weights method). Defaults to 'off'.
 %
-%  Algorithm: The algorithm used for the fitting procedure. Available values are
-%             'Lavenberg-Marquardt', 'Gauss-Newton', or 'Trust-Region'. The
-%             default is 'Trust-Region'.
+%  Algorithm: The algorithm to be used in the fitting. Available values are
+%             'Lavenberg-Marquardt', 'Gauss-Newton', or 'Trust-Region'. Defaults
+%             to 'Trust-Region'.
 %
 %  MovMeanWidth: The window width of the moving average used for smoothing the
-%                curve in order to filter noise before finding the maximas. This
-%                parameter is used only when CenterStart is not given. The value
-%                of this can be set as a positive integer which specifies the
-%                width in terms of the number of data points, OR a real scalar
-%                between [0,1] which specifies the width in terms of a fraction
-%                of the total number of data points.
+%                curve in order to filter out the noise before finding the
+%                maximas. This parameter is used only when CenterStart is not
+%                given. The value of this can be set as a positive integer which
+%                specifies the width in terms of the number of data points, OR a
+%                real scalar between [0,1] which specifies the width as a
+%                fraction of the total number of data points.
 %
 %  DiffMaxChange: The maximum change in coefficients for finite difference
-%                 gradients. The default is 0.1.
+%                 gradients. Defaults to 0.1.
 %
 %  DiffMinChange: The minimum change in coefficients for finite difference
-%                 gradients. The default is 10^-8.
+%                 gradients. Defaults to 10^-8.
 %
-%  MaxFunEvals: The maximum number of evaluations of the model allowed. The
-%               default is 50000.
+%  MaxFunEvals: The allowed maximum number of evaluations of the model. Defaults
+%               to 50000.
 %
-%  MaxIters: The maximum number of iterations allowed for the fit. The default
-%            is 10000.
+%  MaxIters: The maximum number of iterations allowed for the fit. Defaults to
+%            10000.
 %
-%  TolFun: The termination tolerance on the model value. The default is 10^-6.
+%  TolFun: The termination tolerance on the model value. Defaults to 10^-6.
 %
-%  TolX: The termination tolerance on the coefficient values. The default is
-%        10^-6.
+%  TolX: The termination tolerance on the coefficient values. Defaults to 10^-6.
 %
 % Read-only Properties:
 %  Method: The method used for the fitting, which is 'NonLinearLeastSquares'.
@@ -132,21 +135,21 @@ classdef PeakFit
 %
 %  Base: A struct containing the fit results for the baseline.
 %
-%  Area, Center, Height, Width, Baseline: A 3-by-NumPeaks matrix to store the
+%  Area, Center, Height, Width, Baseline: A 3-by-NumPeaks matrix that stores the
 %        fit results for the area,... , respectively. The first row is the
-%        converged values; the second row is the 95% CI lower bounds; and the
-%        third row is the 95% CI upper bounds.
+%        values at convergence; the second row is the 95% CI lower bounds; and
+%        the third row is the 95% CI upper bounds.
 %
-%  RelStDev: Relative Standard Deviation of the fit results.
+%  RelStDev: The relative standard deviation of the fit results.
 %
 %  CoeffDeterm: The coefficient of determination of the fit results.
 %
 %  AdjCoeffDeterm: The degree-of-freedom adjusted coefficient of determination
 %                  of the fit results.
 %
-%  NumFunEvals: Number of function evaluations.
+%  NumFunEvals: The number of function evaluations.
 %
-%  NumIters: Number of iterations.
+%  NumIters: The number of iterations.
 %
 %  ExitFlag: Describes the exit condition of the algorithm. Positive flags
 %            indicate convergence, within tolerances. Zero flags indicate that
@@ -155,11 +158,11 @@ classdef PeakFit
 %            converge to a solution.
 %
 % Public Methods:
-%  disp: Display the options, results, error and performance of the fitting.
+%  disp: Displays the options, results, error and performance of the fitting.
 %
-%  model: Return the reconstructed data points (model) using the fit results.
+%  model: Returns the reconstructed data points (model) using the fit results.
 %
-%  convertunit: Convert the units of the data points and the fit results.
+%  convertunit: Converts the units of the data points and the fit results.
 %               Available units to be converted from or to are:
 %                'eV'        : electron volt
 %                'percm'     : cm^{-1} (wavenumber)
@@ -180,6 +183,7 @@ classdef PeakFit
 % Tested on:
 %  - MATLAB R2013b
 %  - MATLAB R2015b
+%  - MATLAB R2017b
 %
 % Copyright: Herianto Lim
 % http://heriantolim.com/
